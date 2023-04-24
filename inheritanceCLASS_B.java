cff35fe33d914092b78cb1a49c47d915a5d09a98
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-04-24 11:25:38 +0530

    modified files

--- inheritanceCLASS_B.java
+++ inheritanceCLASS_B.java
@@ -1,10 +1,10 @@
 public class inheritanceCLASS_B extends inheritanceCLASS_A{
-        int d1=30;
-        int d2=40;
+        int d3=30;
+        int d4=40;
         public void fun(){
             System.out.println("Inside fun B");
         }
-        public void fun1(){
-            System.out.println("inside fun1 B");
+        public void fun2(){
+            System.out.println("inside fun2 B");
         }
 }
--- inheritanceCLASS_C.java
+++ inheritanceCLASS_C.java
@@ -8,8 +8,20 @@ public class inheritanceCLASS_C {
         a.fun1();*/
 
         //CASE 2
-        inheritanceCLASS_A a=new inheritanceCLASS_B();
-        System.out.println(a.d1);
-        a.fun();
+        // inheritanceCLASS_A a=new inheritanceCLASS_B();
+        // System.out.println(a.d1);
+        // System.out.println(((inheritanceCLASS_B)a).d3);
+        // a.fun();
+        // a.fun1();
+        // ((inheritanceCLASS_B)a).fun2();
+
+        //CASE 3 - error bcoz JVM right side mei dekhega and A class mei d3 and d4 nhi h so on..........
+        //inheritanceCLASS_B a=new inheritanceCLASS_A();
+
+        //Case 4
+        inheritanceCLASS_B b=new inheritanceCLASS_B();
+        System.out.println(b.d1);
+        System.out.println(b.d3);
+        b.fun();
     }
 }
