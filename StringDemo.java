e439de3c2149e6b8cbdfbb84425812663a4277aa
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-04-25 09:40:50 +0530

    files added

---
+++ StringDemo.java
@@ -0,0 +1,8 @@
+public class StringDemo {
+    public static void main(String[] args) {
+        String s="Yo ";
+        // System.out.println(s);
+        s+="Sup";
+        System.out.println(s);
+    }
+}
