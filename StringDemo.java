9bb3848b381f7fbe529a84cf3589af904915cd4a
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-04-25 14:41:22 +0530

    modified

--- StringDemo.java
+++ StringDemo.java
@@ -13,5 +13,13 @@ public class StringDemo {
         System.out.println(a.length());
         System.out.println(a.charAt(0));
         System.out.println(a.compareTo(b)); //returns difference
+        System.out.println(a.indexOf('b'));
+        System.out.println(a.toUpperCase());
+        System.out.println(a.substring(1));
+        System.out.println(a.substring(0, a.length()-1));
+        System.out.println("SUBSTRING=> ");
+        for(int i=0;i<a.length();i++){
+            System.out.println(a.substring(i));
+        }
     }
 }
