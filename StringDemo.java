b37dbc20abd808a42a7ac598ea8f660fa352dfa9
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-04-25 10:00:14 +0530

    modified

--- StringDemo.java
+++ StringDemo.java
@@ -4,5 +4,12 @@ public class StringDemo {
         // System.out.println(s);
         s+="Sup";
         System.out.println(s);
+        String a="abc";
+        String b="abc";
+        String c=new String ("abc");
+        System.out.println(a==b); //Double = will compare the address
+        System.out.println(a==c);
+        System.out.println(a.equals(c));
+        System.out.println(a.length());
     }
 }
