d0872be69d53a18cbbebd9fcb5143b8084cceef0
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-05-04 14:36:20 +0530

    new files added

---
+++ reverseString.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class reverseString {
+    public static void main(String[] args) {
+        try (Scanner ob = new Scanner(System.in)) {
+            String n=ob.nextLine();
+            n=' '+n;
+            String j="";
+            int len=n.length();
+            int i;
+            for(i=len-1;i>=0;i--){
+                if(n.charAt(i)==' '){
+                    System.out.print(j+" ");
+                    j="";
+                }
+                else{
+                    j=n.charAt(i)+j;
+                }
+            }
+            ob.close();
+        }
+    }
+}
---
+++ try_CatchDemo.java
@@ -0,0 +1,38 @@
+public class try_CatchDemo {
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        int a=10;
+        String s="";
+        try{
+            int k=a/0;
+            arr[7]=28;
+            System.out.println(k);
+            System.out.println(s.length());
+        }
+        catch(ArithmeticException e){
+            e.printStackTrace();
+        }
+        catch(NullPointerException e){
+            e.printStackTrace();
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+        finally{ //this block will run irrespective of the exception
+            System.out.println("Finally");
+            try{
+                arr[7]=28;
+                System.out.println(s.length());
+            }
+            catch(ArithmeticException e){
+                e.printStackTrace();
+            }
+            catch(NullPointerException e){
+                e.printStackTrace();
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
