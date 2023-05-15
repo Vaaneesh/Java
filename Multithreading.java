77b406ed283ef8d7417c86e8ab89a2d713ed1e84
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-05-15 15:33:52 +0530

    modified

--- Multithreading.java
+++ Multithreading.java
@@ -1,10 +1,22 @@
-public class Multithreading extends thread {
+public class Multithreading implements Runnable{
     public void run(){
-        System.out.println("running....");
+        for (int i = 0; i < 5; i++) {
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){
+                System.out.println(e);
+            }
+            System.out.println(i);
+        }
+        System.out.println("Running...");
     }
+
     public static void main(String[] args) {
-        Multithreading m1= new Multithreading();
-        m1.run();
-        
+        Multithreading m1 = new Multithreading();
+        Thread t1 =new Thread(m1);
+        Thread t2 =new Thread(m1);
+
+        t1.start();
+        t2.start();
     }
 }
