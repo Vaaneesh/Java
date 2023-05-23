c19c21a3b317cdaa3a81157dae297f675a384754
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-05-23 13:55:48 +0530

    new file added

---
+++ car.java
@@ -0,0 +1,13 @@
+public class car {
+    int price;
+    int speed;
+    int color;
+    public car(){
+
+    }
+    public car(int price,int speed,int color){
+        this.price=price;
+        this.color=color;
+        this.speed=speed;
+    }
+}
---
+++ carClient.java
@@ -0,0 +1,5 @@
+public class carClient {
+    public static void main(String[] args) {
+        
+    }
+}
