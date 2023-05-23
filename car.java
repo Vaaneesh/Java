235284941d8c521b7bfc40f4beb424dc69c9fc8d
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-05-23 14:11:46 +0530

    modified

--- car.java
+++ car.java
@@ -9,7 +9,18 @@ public class car {
 
     public car(int price,int speed,String color){
         this.price=price;
-        this.color=color;
         this.speed=speed;
+        this.color=color;
+
+    }
+
+    @Override
+    public String toString(){
+        return "P: "+this.price+" S: "+this.speed+" C: "+this.color;
+    }
+    void display(car arr[]){
+        for(int i=0;i<5;i++){
+            System.out.println(arr[i]);
+        }
     }
 }
--- carClient.java
+++ carClient.java
@@ -1,11 +1,12 @@
 public class carClient {
     public static void main(String[] args) {
         car arr[]=new car[5];
+        car c=new car();
         arr[0]=new car(1200,100,"white");
         arr[1]=new car(2000,150,"black");
         arr[2]=new car(1500,110,"blue");
         arr[3]=new car(2200,170,"red");
         arr[4]=new car(3200,200,"gravity grey");
-
+        c.display(arr);
     }
 }
