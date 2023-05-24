67b3af1ffd1720a4fd3292a9650609af6c7e9807
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-05-24 15:35:23 +0530

    modified

--- Hashmap.java
+++ Hashmap.java
@@ -3,9 +3,13 @@ import java.util.HashMap;
 public class Hashmap {
     public static void main(String[] args) {
         HashMap<Integer,String> map=new HashMap<>();
-        map.put(3,"yo");
+        map.put(3,"what");
         map.put(2,"sup");
-        map.put(1, "hello");
+        map.put(1, "yo");
+        map.put(4, "you");
+        map.put(5, "doing");
         System.out.println(map);        
+        System.out.println(map.get(2));
+        System.out.println(map.containsKey(20));
     }
 }
