4e9e10fd3998ea737bda62e508e859707c839a57
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-05-24 15:25:01 +0530

    Hashmap file added

---
+++ Hashmap.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class Hashmap {
+    public static void main(String[] args) {
+        HashMap<Integer,String> map=new HashMap<>();
+        map.put(3,"yo");
+        map.put(2,"sup");
+        map.put(1, "hello");
+        System.out.println(map);        
+    }
+}
