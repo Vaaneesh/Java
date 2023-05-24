c0f5088770c88ba1e68996c80bd598edee28dda4
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-05-24 15:44:27 +0530

    modified

--- Hashmap.java
+++ Hashmap.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.*;
 
 public class Hashmap {
     public static void main(String[] args) {
@@ -11,5 +12,11 @@ public class Hashmap {
         System.out.println(map);        
         System.out.println(map.get(2));
         System.out.println(map.containsKey(20));
+        System.out.println(map.keySet()); //helps in Loop
+        System.out.println("        Loop Starts         ");
+        Set <Integer>set=map.keySet();
+        for(int a:set){
+            System.out.println(a+": "+map.get(a));
+        }
     }
 }
