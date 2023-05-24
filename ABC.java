adffb1122878783a0fa09dc7bc0ed9ea6b05045f
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-05-24 15:13:05 +0530

    deleted

--- ABC.java
+++
@@ -1,9 +0,0 @@
-public class ABC {
-    public <T> ABC(T a){
-        System.out.println(a);
-    }
-    public static void main(String[] args) {
-        ABC a=new ABC("hello");
-        ABC a1=new ABC(13);
-    }
-}
