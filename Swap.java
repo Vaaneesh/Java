f649fc491c73cc25afee5a436012c2ffd382b3a6
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-01 17:45:32 +0530

    changes done

--- Swap.java
+++ Swap.java
@@ -1,11 +1,12 @@
 import java.util.*;
 public class Swap
 {
-    public static void swap(int arr[],int a,int b){
-        int temp=arr[a];
-        arr[a]=arr[b];
-        arr[b]=temp;
+    public static void swap(int arr[],int arr2[]){
+        int []temp=arr;
+        arr=arr2;
+        arr2=temp;
         System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arr2));
     }
 	public static void main(String[] args) {
 		Scanner ob=new Scanner(System.in);
@@ -14,10 +15,15 @@ public class Swap
 		for(int i=0;i<5;i++){
 		    arr[i]=ob.nextInt();
 		}
-		System.out.println("enter 2 keys");
-		int a=ob.nextInt();
-		int b=ob.nextInt();
-		swap(arr,a,b);
+		 System.out.println("enter elements");
+		int arr2[]=new int[5];
+		for(int i=0;i<5;i++){
+		    arr2[i]=ob.nextInt();
+		}
+		// System.out.println("enter 2 keys");
+		// int a=ob.nextInt();
+		// int b=ob.nextInt();
+		swap(arr,arr2);
         ob.close();
 	}
 }
