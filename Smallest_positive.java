f882a041e6923222537b3a5a8e1e42c5af1435f7
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-07 22:58:20 +0530

    file added

---
+++ Smallest_positive.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Smallest_positive {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int arr[]=new int[5];
+        System.out.println("enter elements");
+        for(int i=0;i<5;i++){
+            arr[i]=ob.nextInt();
+        }
+        Arrays.sort(arr);
+        int a=1;
+        for(int i=0;i<5;i++){
+            if(arr[i]>0 && a==arr[i])
+            a++;
+        }
+        System.out.println(a);
+        ob.close();
+    }
+}
