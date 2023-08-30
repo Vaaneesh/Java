69c9ee87d7e837dffac0bd5ec8952d39b86edae2
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-08-30 18:23:49 +0530

    changes made

--- subsequence_Recursion.java
+++ subsequence_Recursion.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class subsequence_Recursion {
-    
+    private static void subseq(String s,String ans){
+        if(s.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        subseq(s.substring(1),ans);
+        subseq(s.substring(1),ans+s.charAt(0));
+    }
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter String");
+        String s=ob.next();
+        subseq(s, "");
+        ob.close();
+    }
 }
--- toss.java
+++ toss.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class toss {
-    public static void toss(int n,String ans){
+    public static void toss2(int n,String ans){
         if(n==0){
             System.out.println(ans);
             return;
         }
         if(ans.length()==0 || ans.charAt(ans.length()-1)!='H'){
-           toss(n-1,ans+"H");
+           toss2(n-1,ans+"H");
         }
-        toss(n-1,ans+"T");
+        toss2(n-1,ans+"T");
     }
     public static void main(String[] args) {
         Scanner ob=new Scanner(System.in);
         System.out.println("Enter no of toss");
         int n=ob.nextInt();
-        toss(n,"");
+        toss2(n,"");
         ob.close();
     }
 }
