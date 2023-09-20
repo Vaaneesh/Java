fbe3af11dca316672b75a15d515b411fa08af261
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-20 18:31:54 +0530

    file added

---
+++ timeComp.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class timeComp {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        int i=1;
+        while(i<=n){
+            System.out.println("hey");
+            i*=2;
+        }
+        ob.close();
+    }
+}
