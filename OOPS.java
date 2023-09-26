10b8c14d36e55017d6dcc14f9bad27285233f7c5
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-09-26 16:55:42 +0530

    2 files added

---
+++ OOPS.java
@@ -0,0 +1,11 @@
+public class OOPS {
+    String name="Vasu";
+    int roll=1483;
+    OOPS(String name,int roll){
+        this.name=name;
+        this.roll=roll;
+    }
+    public void introduction(){
+        
+    }
+} 
---
+++ OOPS_client.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class OOPS_client{    
+    public static void main(String[] args) {
+        OOPS s=new OOPS("ABcd",1420);
+        System.out.println(s.name);
+        System.out.println(s.roll);
+    }
+}
