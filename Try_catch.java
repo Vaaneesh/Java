12ae1f73c2a95c666b5f1bda2825ab681db27393
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-10-17 19:38:14 +0530

    file added

---
+++ Try_catch.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Try_catch {
+    public static void main(String[] args) {
+        Scanner ob=new Scanner(System.in);
+        System.out.println("Enter value of a and b");
+        int a=ob.nextInt();
+        int b=ob.nextInt();
+        try{
+            int c=a/b;
+            System.out.println("Result=> "+c);
+        }
+        catch(Exception e){
+            System.out.println(e.getClass().getName());
+        }
+        ob.close();
+    }
+}
