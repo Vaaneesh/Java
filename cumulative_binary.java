ac236fdb80b80cc627df9d2512e68436fe3d46ca
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-10-23 13:04:39 +0530

    changes made

--- cumulative_binary.java
+++ cumulative_binary.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class cumulative_binary {
-    public static int countbits(int n){
-        int c=0;
-        while(n!=0){
-            if((n&1)==1){
-                c++;
-            }
-            n=n>>1;
-        }
-        return c;
-    }
+    // public static int countbits(int n){
+    //     int c=0;
+    //     while(n!=0){
+    //         if((n&1)==1){
+    //             c++;
+    //         }
+    //         n=n>>1;
+    //     }
+    //     return c;
+    // }
     public static void main(String[] args) {
         Scanner ob=new Scanner(System.in);
         System.out.println("enter number");
         int n=ob.nextInt();
         int sum=0;
         for(int i=1;i<=n;i++){
-            sum+=countbits(i);
+            sum+=Integer.bitCount(i);
         }
         System.out.println(sum);
         ob.close();
