59878741ec427326771b8da34e39c8b5be7e3faf
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-10-26 00:39:11 +0530

    modified

--- balanced_para.java
+++ balanced_para.java
@@ -2,27 +2,28 @@ import java.util.*;
 
 public class balanced_para {
     public static void main(String[] args) {
-        Scanner ob=new Scanner(System.in);
-        System.out.println("Enter string containing paraenthesis");
-        String s=ob.next();
-        char ch[]=s.toCharArray();
-        Stack<Character> st=new Stack<Character>();
-        for(char c:ch){
-            if(c=='(' || c=='{' || c=='['){
-                st.push(c);
-            }
-            else{
-                if((st.peek()=='{') && c=='}' || (st.peek()=='[') && c==']' || (st.peek()=='(')&& c==')'){
-                    st.pop();
+        try (Scanner ob = new Scanner(System.in)) {
+            System.out.println("Enter string containing paraenthesis");
+            String s=ob.next();
+            char ch[]=s.toCharArray();
+            Stack<Character> st=new Stack<Character>();
+            for(char c:ch){
+                if(c=='(' || c=='{' || c=='['){
+                    st.push(c);
+                }
+                else{
+                    if((st.peek()=='{') && c=='}' || (st.peek()=='[') && c==']' || (st.peek()=='(')&& c==')'){
+                        st.pop();
+                    }
+                }
+                if(st.empty()){
+                    System.out.println("True");
+                    return;
                 }
             }
-            if(st.empty()){
-                System.out.println("True");
-                return;
-            }
+            System.out.println("false");
+            ob.close();
         }
-        System.out.println("false");
-        ob.close();
     }
 }
 
